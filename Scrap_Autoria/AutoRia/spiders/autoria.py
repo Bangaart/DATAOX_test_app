367d87047a6c51752a869f4b0d768b8e54803a94
Arthur <planck2909@gmail.com>
2026-01-27 19:38:14 +0200

    Added all fields except phone number

--- Scrap_Autoria/AutoRia/spiders/autoria.py
+++ Scrap_Autoria/AutoRia/spiders/autoria.py
@@ -2,9 +2,40 @@ import scrapy
 
 
 class AutoriaSpider(scrapy.Spider):
-    name = "autoria_used"
+    name = "autoria"
     allowed_domains = ["auto.ria.com"]
-    start_urls = ["https://auto.ria.com/used"]
+    start_urls = ["https://auto.ria.com/uk/car/used/"]
+    custom_settings = {
+        "FEEDS": {
+        "cars.json": {
+        "format": "json",
+        "encoding": "utf-8",
+        "indent": 4,
+        "override": True
+    }
+},
+        "COOKIES_ENABLED": False
+    }
+
 
     def parse(self, response):
-        pass
+        cars_links = response.xpath('//div[@class="content"]//a/@href').re(r"^https:\/\/.+$")
+        yield from response.follow_all(cars_links, callback=self.parse_car_item)
+
+        # next_url = response.xpath('//a[@class="page-link"]').attrib['href']
+        # yield from response.follow_all(next_url, callback=self.parse)
+
+    def parse_car_item(self, response):
+        for item  in response.xpath('//div[@id="main"]'):
+            yield {
+                "url": response.url,
+                "title": item.xpath('//div[@id="sideTitleTitle"]/span/text()').get(),
+                "price_usd": item.xpath('//div[@id="sidePrice"]/strong/text()').get(),
+                "odometer":  item.xpath('//div[@id="basicInfoTableMainInfo0"]/span/text()').get(),
+                "username": item.xpath('//div[@id="sellerInfoUserName"]/span/text()').get(),
+                "phone_number": item.xpath('//div[@id="sellerInfoPhoneNumber"]/text()').get(default="Not found"),
+                "image_url": item.css(".photo-slider").xpath('//img/@src').re("^https:\/\/.+$")[0] ,
+                "images_count": item.xpath('//div[@id="photoSlider"]/span/span[last()]/text()').get(),
+                "car_number": item.xpath('//div[@id="badges"]/div/span/text()').get(default="Number is not provided").strip(),
+                "car_vin": item.xpath('//div[@id="badgesVinGrid"]//span/text()').get(),
+            }
